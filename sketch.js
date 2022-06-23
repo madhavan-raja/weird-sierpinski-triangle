1a63f24c88ea8210d985fa66d29ec3eab8ecfb9b
Madhavan Raja <madhavanraja99@gmail.com>
2022-06-23 20:38:36 +0530

    Implemented everything

--- sketch.js
+++ sketch.js
@@ -2,6 +2,10 @@ const width = 600;
 const height = 600;
 const edges = 3;
 const shapeSize = 250;
+const pointsPerFrame = 1000;
+
+let corners = [];
+let previousPoint = undefined;
 
 function setup() {
 	var canvas = createCanvas(width, height);
@@ -9,26 +13,56 @@ function setup() {
 	const y = (windowHeight - height) / 2;
 	canvas.position(x, y);
 
+	translate(width / 2, height / 2);
+
 	background(46);
 
 	noFill();
 	stroke(255);
-	strokeWeight(4);
+	strokeWeight(2);
 
-	polygon(width / 2, height / 2, shapeSize, edges);
+	drawPolygon();
+
+	previousPoint = randomPointInPolygon();
+
+	frameRate(60);
 }
 
 function draw() {
+	translate(width / 2, height / 2);
+
+	for (let i = 0; i < pointsPerFrame; ++i) {
+		point(previousPoint[0], previousPoint[1]);
+		let randomCornerIndex = random(edges);
+		let newPoint = midPoint(random(corners), previousPoint);
+		previousPoint = newPoint;
+	}
 }
 
-function polygon(x, y, radius, npoints) {
+function drawPolygon() {
 	const offset = PI / 2;
-	let angle = TWO_PI / npoints;
-	beginShape();
+	let angle = TWO_PI / edges;
+
 	for (let a = -offset; a < TWO_PI - offset; a += angle) {
-		let sx = x + cos(a) * radius;
-		let sy = y + sin(a) * radius;
-		vertex(sx, sy);
+		let sx = cos(a) * shapeSize;
+		let sy = sin(a) * shapeSize;
+
+		point(sx, sy);
+
+		corners.push([sx, sy]);
 	}
-	endShape(CLOSE);
+}
+
+function randomPointInPolygon() {
+	let angle = random(TWO_PI);
+	let length = shapeSize * sin(PI * ((1 / 2) - (1 / edges)));
+
+	return [sin(angle) * length, cos(angle) * length]
+}
+
+function midPoint(p1, p2) {
+	return [
+		(p1[0] + p2[0]) / 2,
+		(p1[1] + p2[1]) / 2
+	];
 }
